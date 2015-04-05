fce98f6695130b54bbb221bbae559c85b7a9ee61
Jason <konk353535@hotmail.com>
2015-04-05 23:04:24 +1000

    getGameData firebase pull removed, pull from mongo added

--- server.js
+++ server.js
@@ -32,42 +32,6 @@ var ChampionSchema = mongoose.Schema({
 var Champion = mongoose.model('Champion', ChampionSchema);
 
 
-function champSaved(){
-	Champion.find(function (err, Champion) {
-	  if (err) return console.error(err);
-	  console.log(Champion)
-	});
-}
-createInitial();
-function createInitial(){
-
-	request('https://global.api.pvp.net/api/lol/static-data/oce/v1.2/champion?api_key=' + config.apikey , function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			console.log(body);
-			// Once game list recieved, pass to game handler
-			
-			champions = JSON.parse(body);
-
-			champions = champions["data"];
-
-			for(var champion in champions){
-				champion_info = champions[champion];
-
-		    		champ_id = champion_info["id"];
-		    		champ_name = champion_info["name"];
-		    		console.log(champ_id);
-		    		
-		    		var Teemo = new Champion({name: champ_name, wins: 0, losses: 0, id: champ_id});
-
-				Teemo.save(function (err, Teemo) {
-				  if (err) return console.error(err);
-				  champSaved();
-				});
-				
-		    	}
-		}
-	});
-}
 
 // Vars for our timer
 var NanoTimer = require('nanotimer');
@@ -100,17 +64,11 @@ function liftOff(timer){
 
 app.get('/getGameData', function(req, res){
 	
-	var ref = new Firebase("https://boiling-inferno-4886.firebaseio.com/champion");
-	
-	ref.once("value", function(snapshot) {
-	  // Success
-	  res.json(snapshot.val());
-	  
-	}, function (errorObject) {
-	  console.log("The read failed: " + errorObject.code);
-	  //callback("Failure", null);
+	Champion.find(function (err, Champion) {
+	  if (err) return console.error(err);
+	  res.json(Champion)
 	});
-	
+
 });
 
 
