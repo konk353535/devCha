60be73aa00f82336a3c5061e1ad39d37f69ab96d
Jason <konk353535@hotmail.com>
2015-04-05 22:57:42 +1000

    Added id filed to champion schema

--- server.js
+++ server.js
@@ -26,23 +26,48 @@ var ChampionSchema = mongoose.Schema({
     name  :  { type: String, default: 'hahaha' }
   , wins   :  { type: Number, min: 0, index: true }
   , losses   :  { type: Number, min: 0, index: true }
+  , id   :  { type: Number, min: 0, index: false }
 });
 
 var Champion = mongoose.model('Champion', ChampionSchema);
 
-var Teemo = new Champion({name: 'Darius', wins: 0, losses: 0});
 
-Teemo.save(function (err, Teemo) {
-  if (err) return console.error(err);
-  champSaved();
-});
 function champSaved(){
 	Champion.find(function (err, Champion) {
 	  if (err) return console.error(err);
 	  console.log(Champion)
 	});
 }
+createInitial();
+function createInitial(){
 
+	request('https://global.api.pvp.net/api/lol/static-data/oce/v1.2/champion?api_key=' + config.apikey , function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+			// Once game list recieved, pass to game handler
+			
+			champions = JSON.parse(body);
+
+			champions = champions["data"];
+
+			for(var champion in champions){
+				champion_info = champions[champion];
+
+		    		champ_id = champion_info["id"];
+		    		champ_name = champion_info["name"];
+		    		console.log(champ_id);
+		    		
+		    		var Teemo = new Champion({name: champ_name, wins: 0, losses: 0, id: champ_id});
+
+				Teemo.save(function (err, Teemo) {
+				  if (err) return console.error(err);
+				  champSaved();
+				});
+				
+		    	}
+		}
+	});
+}
 
 // Vars for our timer
 var NanoTimer = require('nanotimer');
