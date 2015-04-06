3aa1e0d15898e20dbb0d0f1824fabdb944248aad
Jason <konk353535@hotmail.com>
2015-04-06 16:33:11 +1000

    Initally loads game d data in our angular controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,22 +4,31 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
 // Pulls list of games
 $scope.pullGame = function() {
-  console.log("Here");
-  $http.get('/getGameData').success(function(response) {
-    console.log(response);
-    $scope.championlist = response;
-  });
-
+	getGameData();
 };  
+
+getGameData();
 
 // Orders by total games
 $scope.getTotalGames = function(champion) {
-   return champion.wins + champion.losses;
+	return champion.wins + champion.losses;
 };
 
+$scope.getTotal = function(){
+	var total = 0;
+	for(var i = 0; i < $scope.championlist.length; i++){
+	    var champion = $scope.championlist[i];
+	    total += (champion.wins + champion.losses);
+	}
+	return total;
+}
 
-
-
+function getGameData(){
+	$http.get('/getGameData').success(function(response) {
+		console.log(response);
+		$scope.championlist = response;
+	});
+}
 
 
 }]);
